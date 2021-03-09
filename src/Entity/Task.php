7ce29cd23ae58a734702c49450f17c7a6559c591
Expatgit <m.kropp003@gmail.com>
2021-03-09 16:28:54 +0300

    9.03 создана таблица task с ВК на id к USER

---
+++ src/Entity/Task.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\TaskRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=TaskRepository::class)
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $text;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $created_time;
+
+    /**
+     * @ORM\Column(type="datetime", nullable=true)
+     */
+    private $update_time;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class)
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(?string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(?string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+
+    public function getCreatedTime(): ?\DateTimeInterface
+    {
+        return $this->created_time;
+    }
+
+    public function setCheatedTime(?\DateTimeInterface $created_time): self
+    {
+        $this->created_time = $created_time;
+
+        return $this;
+    }
+
+    public function getUpdateTime(): ?\DateTimeInterface
+    {
+        return $this->update_time;
+    }
+
+    public function setUpdateTime(?\DateTimeInterface $update_time): self
+    {
+        $this->update_time = $update_time;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
